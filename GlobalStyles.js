ce653063efedf25967400dd158474bcf147fc4cd
Nurfitriana <nurfitriana.job@gmail.com>
2023-01-01 12:00:00 +0700

    Initial commit

---
+++ GlobalStyles.js
@@ -0,0 +1,54 @@
+/* fonts */
+export const FontFamily = {
+  heiReina: "Hei Reina",
+  trajanPro: "Trajan Pro",
+  baskervilleOldFace: "Baskerville Old Face",
+  roboto: "Roboto",
+  inter: "Inter",
+  baskervville: "Baskervville",
+};
+/* font sizes */
+export const FontSize = {
+  size_11xl: 30,
+  size_mini: 15,
+  size_6xl: 25,
+  size_3xs: 10,
+  size_xs: 12,
+  size_mid: 17,
+  size_xl: 20,
+  size_5xs: 8,
+  size_21xl: 40,
+  size_31xl: 50,
+  size_sm: 14,
+  size_7xs: 6,
+};
+/* Colors */
+export const Color = {
+  colorGray_100: "#fbfdfd",
+  colorGray_200: "#8b8b8b",
+  colorGray_300: "#787878",
+  colorGray_400: "#1e1e1e",
+  colorGray_700: "rgba(10, 10, 10, 0.7)",
+  colorGray_800: "rgba(0, 0, 0, 0.7)",
+  colorGray_900: "rgba(0, 0, 0, 0.5)",
+  colorGray_600: "rgba(0, 0, 0, 0.8)",
+  colorGray_500: "rgba(0, 0, 0, 0.3)",
+  colorGray_1000: "rgba(0, 0, 0, 0.65)",
+  colorLimegreen: "#2bb32a",
+  colorYellow: "#fffa3c",
+  colorBlack: "#000",
+  colorGold_100: "#fbe343",
+  colorGold_200: "#e9c005",
+  colorWhitesmoke: "#efefef",
+  colorGoldenrod: "#dfb805",
+  colorWhite: "#fff",
+  colorMoccasin_100: "#fce2b3",
+  colorPalegoldenrod: "rgba(253, 255, 181, 0.8)",
+};
+/* border radiuses */
+export const Border = {
+  br_11xl: 30,
+  br_3xs: 10,
+  br_8xs: 5,
+  br_mini: 15,
+};
